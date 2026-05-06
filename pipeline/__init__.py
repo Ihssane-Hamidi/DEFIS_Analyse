9684adb93ece77989a263f0ed683f9c21512ecb0
Ihssane-Hamidi <122749818+Ihssane-Hamidi@users.noreply.github.com>
2026-05-06 12:16:52 +0200

    Delete pipeline directory

--- pipeline/__init__.py
+++
@@ -1,5 +0,0 @@
-"""pipeline/__init__.py"""
-from .builder import run_pipeline
-from .parsers import get_parser, REQUIRED_COLS
-from .drive   import save_parquet, load_parquet, drive_is_configured
-from .common  import build_periods
